3a0565f5b3700f8fc22bf483cc8151c657caac4c
Ethan <ebonsignori@gmail.com>
2019-04-05 02:40:30 -0400

    create account/login

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -14,3 +14,31 @@ signupForm.on('submit', (e) => {
       console.log(cred)
     })
 })
+
+// Toggle password visibility with icon by switching it from type=password to type=text
+$('.password-toggle').on('click', function () {
+  $(this).children('i').toggleClass('fa-eye fa-eye-slash')
+  let pwInput = $('.password')
+  let type = pwInput.attr('type')
+  if (type === 'password') {
+    pwInput.attr('type', 'text')
+  } else {
+    pwInput.attr('type', 'password')
+  }
+})
+
+// Get user keyup from password field to check if both password fields match
+$('#signup-password, #signup-password-confirm').on('keyup', function () {
+  let pw1 = $('#signup-password')
+  let pw2 = $('#signup-password-confirm')
+  let pwResponse = $('#password-response')
+  if (pw1.val() === pw2.val()) {
+    pwResponse.html('Password match').css('color', 'green')
+    $('#match').css('display', 'block')
+    $('#no-match').css('display', 'none')
+  } else {
+    pwResponse.html('Passwords do not match').css('color', 'red')
+    $('#no-match').css('display', 'block')
+    $('#match').css('display', 'none')
+  }
+})
